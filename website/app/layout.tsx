29cc9f4b9ce64416370550ac100745c7480e72c0
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-24 11:42:55 -0500

    Add metadata

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -5,14 +5,8 @@ import { ThemeProvider } from '@/providers/theme';
 import { Analytics } from '@vercel/analytics/next';
 import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
-import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import './globals.css';
-
-export const metadata: Metadata = {
-  title: 'Orate',
-  description: 'The AI toolkit for speech.',
-};
 
 type RootLayoutProps = {
   children: ReactNode;
--- website/app/page.tsx
+++ website/app/page.tsx
@@ -4,6 +4,47 @@ import { Hero } from '@/components/hero';
 import { Providers } from '@/components/providers';
 import { SpeechToText } from '@/components/speech-to-text';
 import { TextToSpeech } from '@/components/text-to-speech';
+import type { Metadata } from 'next';
+
+const title = 'Orate | The AI toolkit for speech';
+const description =
+  'Create realistic, human-like speech and transcribe audio with a unified API that works with leading AI providers like OpenAI, ElevenLabs and AssemblyAI.;';
+const url = 'https://www.orate.dev';
+
+export const metadata: Metadata = {
+  title,
+  description,
+  authors: [
+    {
+      name: 'Hayden Bleasel',
+      url: 'https://www.haydenbleasel.com',
+    },
+  ],
+  formatDetection: {
+    telephone: false,
+  },
+  creator: 'Hayden Bleasel',
+  keywords: ['speech', 'ai', 'toolkit', 'text-to-speech', 'speech-to-text'],
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: title,
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    title,
+    description,
+    creator: '@haydenbleasel',
+    card: 'summary_large_image',
+  },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title,
+  },
+};
 
 const Home = () => (
   <>
