2887a98af8164996c259bf57c15af70a0b3261f9
Hayden Bleasel <hello@haydenbleasel.com>
2025-01-28 11:40:00 -0500

    Add test script

--- __tests__/elevenlabs.test.ts
+++ __tests__/elevenlabs.test.ts
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from 'node:fs/promises';
 import { describe, expect, it } from 'vitest';
-import { change, speak } from '../src';
+import { change, isolate, speak } from '../src';
 import { elevenlabs } from '../src/elevenlabs';
 
 describe('ElevenLabs Tests', () => {
@@ -48,6 +48,24 @@ describe('ElevenLabs Tests', () => {
 
     await writeFile(
       './__tests__/output/elevenlabs-speech-changed.mp3',
+      Buffer.from(await speech.arrayBuffer())
+    );
+
+    expect(speech).toBeInstanceOf(File);
+    expect(speech.size).toBeGreaterThan(0);
+  });
+
+  it('should isolate speech', async () => {
+    const file = await readFile('./__tests__/noise.mp3');
+    const audio = new File([file], 'noise.mp3', { type: 'audio/mp3' });
+
+    const speech = await isolate({
+      model: elevenlabs.isl(),
+      audio,
+    });
+
+    await writeFile(
+      './__tests__/output/elevenlabs-speech-isolated.mp3',
       Buffer.from(await speech.arrayBuffer())
     );
 
