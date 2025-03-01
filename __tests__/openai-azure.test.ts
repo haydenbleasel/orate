94e24fe4cb7584e9ff600155609d8539b1f49545
Hayden Bleasel <hello@haydenbleasel.com>
2025-02-28 21:22:31 -0500

    Create openai-azure.test.ts

---
+++ __tests__/openai-azure.test.ts
@@ -0,0 +1,34 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import { describe, expect, it } from 'vitest';
+import { speak, transcribe } from '../src';
+import { openaiAzure } from '../src/openai.azure';
+
+describe('OpenAI Tests', () => {
+  it('should convert text to speech', async () => {
+    const speech = await speak({
+      model: openaiAzure.tts('tts-1', 'ash'),
+      prompt: 'Friends, Romans, countrymen, lend me your ears!',
+    });
+
+    await writeFile(
+      './website/public/examples/tts/openai.wav',
+      Buffer.from(await speech.arrayBuffer())
+    );
+
+    expect(speech).toBeInstanceOf(File);
+    expect(speech.size).toBeGreaterThan(0);
+  });
+
+  it('should convert speech to text', async () => {
+    const file = await readFile('./__tests__/test.mp3');
+    const audio = new File([file], 'test.mp3', { type: 'audio/mp3' });
+
+    const text = await transcribe({
+      model: openaiAzure.stt('whisper-1'),
+      audio,
+    });
+
+    expect(typeof text).toBe('string');
+    expect(text.length).toBeGreaterThan(0);
+  });
+});
