ffa83876b83b693f671e0e4bd0a3ac839a9775f0
Hayden Bleasel <hello@haydenbleasel.com>
2025-03-01 00:00:12 -0500

    Finish splitting up pages

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -5,7 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  redirects: [
+  redirects: () => [
     {
       source: '/docs/features',
       destination: '/docs/features/tts',
