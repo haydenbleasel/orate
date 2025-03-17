1e2d2d8a5ef5a26ab16435b0e3fbfa2b2c595422
Hayden Bleasel <hello@haydenbleasel.com>
2025-03-17 11:03:41 -0700

    Create neuphonic.test.ts

---
+++ __tests__/neuphonic.test.ts
@@ -0,0 +1,23 @@
+import { writeFile } from 'node:fs/promises';
+import { describe, expect, it } from 'vitest';
+import { speak } from '../src';
+import { Neuphonic } from '../src/neuphonic';
+
+describe('Neuphonic Tests', () => {
+  const neuphonic = new Neuphonic();
+
+  it('should convert text to speech', async () => {
+    const speech = await speak({
+      model: neuphonic.tts('James'),
+      prompt: 'Friends, Romans, countrymen, lend me your ears!',
+    });
+
+    await writeFile(
+      './website/public/examples/tts/neuphonic.wav',
+      Buffer.from(await speech.arrayBuffer())
+    );
+
+    expect(speech).toBeInstanceOf(File);
+    expect(speech.size).toBeGreaterThan(0);
+  });
+});
